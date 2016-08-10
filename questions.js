b4db2ac5c99a84718592f5af7e12cecad8a7f77e
Daniel Cavero <danielcavero1@gmail.com>
2016-08-10 17:27:27 -0400

    BIG CHANGES
    Initialized by defining the empty variables name_original and
    lastname_original
    
    Put everything into new functions:
    
    nameinputs() - shows prompts to give value to initial variables (keeps
    them "global" (not really global becuase they are inside the IDCheck
    function)
    
    name() :  pomts the name input and reacts to OK. Cancel, or empty prompt
    and return the name_original value
    
    lastname() promts the name input and reacts to OK. Cancel, or empty
    prompt and return the lastname_original value

--- questions.js
+++ questions.js
@@ -8,73 +8,108 @@ Different people will cause different responses.
 //Definning main function
 
 function IDcheck (){
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	//Definning inner variables
+	var name_original = ''
+	var lastname_original = ''
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////
+	//Definning inner functions
+
+	//Nico's function 
+
+	function nico() {
+	    var contrasena = prompt("Seguro? Hmmm.... cual es tu comida mex favorita?");
+	    var contrasena = contrasena.toLowerCase();
+	    var contrasena = contrasena.replace(/\s/g, '');
+	    if (contrasena == "fajitas" || contrasena == "fajita"){
+				alert("Yeeeaaa!! Habla bro ta la vaina?");
+			} else {
+				alert ("poser");
+			}
+
+	} 
+
+	//Jessi's function
+
+	function jessi() {
+		var contrasena = prompt("Segura? Hmmm.... En que ciudad comimos Koreano por primera vez?");
+		var contrasena = contrasena.toLowerCase();
+		var contrasena = contrasena.replace(/\s/g, '');
+			if (contrasena == "losangeles" || contrasena == "la"){
+				alert("Yeeeaaa!!! Hola Catiraaaa bella!!!\nVamos de viaje juntoooos wujuuuu :)?");
+			} else {
+				alert ("Bai :(");
+			}
+
+	}
+
+	//Unkown person
+
+	function unkown() {
+	    alert ("Hello " + name_original + " "+ lastname_original);
+	}
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-//Definning inner functions
 
-//Nico's function 
 
-function nico() {
-    var contrasena = prompt("Seguro? Hmmm.... cual es tu comida mex favorita?");
-    var contrasena = contrasena.toLowerCase();
-    var contrasena = contrasena.replace(/\s/g, '');
-    if (contrasena == "fajitas" || contrasena == "fajita"){
-			alert("Yeeeaaa!! Habla bro ta la vaina?");
-		} else {
-			alert ("poser");
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Defining of variables (name & lastname) with promps
+	function nameinputs(){
+		function name(){
+			name_original = prompt("Name: ");
+				if (name_original == "" ) {
+					    if (confirm ("Sorry try again. Please type in a name.")) {
+						name();
+					    }
+				} else {
+					return name_original;
+				}
+				}
+		
+		function lastname()	{		
+			lastname_original = prompt("Last name: ");
+				if (lastname_original == "" ) {
+						if (confirm ("Sorry try again. Please type in a last name.")) {
+						lastname();
+						}
+				} else {
+					return lastname_original;
+				}
+				} 
+		name();
+		if (name_original == null) {
+			alert ("Goodbye");
+			return false;
 		}
-
-} 
-
-//Jessi's function
-
-function jessi() {
-   	var contrasena = prompt("Segura? Hmmm.... En que ciudad comimos Koreano por primera vez?");
-	var contrasena = contrasena.toLowerCase();
-	var contrasena = contrasena.replace(/\s/g, '');
-		if (contrasena == "losangeles" || contrasena == "la"){
-			alert("Yeeeaaa!!! Hola Catiraaaa bella!!!\nVamos de viaje juntoooos wujuuuu :)?");
-		} else {
-			alert ("Bai :(");
+		lastname();
+			if (lastname_original == null) {
+			alert ("Goodbye");
+			return false;
 		}
- 
-}
+	}
+	nameinputs();
 
-//Unkown person
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Fixing inputs to lowercase
+	var lastname_lowercase =  lastname_original.toLowerCase();
+	var name_lowercase =  name_original.toLowerCase();
 
-function unkown() {
-    alert ("Hello " + name_original + " "+ lastname_original);
-}
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Removing spaces from imputs
+	var lastname =  lastname_lowercase.replace(/\s/g, '');
+	var name =  name_lowercase.replace(/\s/g, '');
 
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/*Run functions */ 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Defining of variables (name & lastname) with promps
-var name_original = prompt("Primer nombre: ");
-var lastname_original = prompt("Apellido: ");
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Fixing inputs to lowercase
-var lastname_lowercase =  lastname_original.toLowerCase();
-var name_lowercase =  name_original.toLowerCase();
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//Removing spaces from imputs
-var lastname =  lastname_lowercase.replace(/\s/g, '');
-var name =  name_lowercase.replace(/\s/g, '');
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*Run functions */ 
-
-if (name == "jessica" || name == "jessi" || name == "jessy" || name == "jess"  && lastname == "pecoraro") {
-    jessi()
-    if (name == "nicolas" || name == "nico" && lastname == "cavero") {
-        nico();
-
-    }
-
-    else {
-        unkown();
-    }
+	if (name == "jessica" || name == "jessi" || name == "jessy" || name == "jess"  && lastname == "pecoraro") {
+	    jessi();
+	} else if (name == "nicolas" || name == "nico" && lastname == "cavero") {
+	    nico();
+	} else {
+	    unkown();
+	}
 
 }
